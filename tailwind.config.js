d4c9a50e5aa18f9392c54d1ab9db4b6d140c2a2b
RobinsonLaw <koksing1@gmail.com>
2025-06-09 00:09:59 +0800

    Tailwind production mode setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    "./api/templates/**/*.html",
+    // add other paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
