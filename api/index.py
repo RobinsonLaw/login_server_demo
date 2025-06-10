970edb6be4392f2bacd08f9acfb8ab6c43c3e39c
RobinsonLaw <koksing1@gmail.com>
2025-06-10 14:21:26 +0800

    re-structure favicon and tailwind.css run at local

--- api/index.py
+++ api/index.py
@@ -8,7 +8,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 # Initialize Flask app
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 
 # Vercel-specific configuration
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secrets.token_hex(16))
@@ -534,6 +535,21 @@ def web_profile():
     user_posts = Post.query.filter_by(user_id=session['user_id']).order_by(Post.created_at.desc()).all()
     
     return render_template('profile.html', user=user, user_posts=user_posts)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('public', 'favicon.ico')
+@app.route('/favicon.png')
+def favicon_png():
+    return send_from_directory('public', 'favicon.png')
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('public/static', filename)
+# @app.route('/static/<path:filename>')
+# def static_files(filename):
+#     folder = os.path.join(app.root_path, 'public/static')
+#     print("Serving static file:", filename)
+#     print("Looking in folder:", folder)
+#     return send_from_directory(folder, filename)
 # @app.route('/favicon.ico')
 # def favicon():
 #     return send_from_directory(
